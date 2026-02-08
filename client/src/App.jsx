d7fce5b7ab803999d827e49e6bb4717f5d4fab8e
Gourav <gauravchugh1389@gmail.com>
2026-02-08 18:06:34 +0000

    fix: Remove login screen for demo, direct to home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Login } from './pages/Login';
 import { Scanner } from './pages/Scanner';
 import { ProductSearch } from './components/ProductSearch';
 
 function App() {
-  const [user, setUser] = useState(null);
-
   return (
     <Router basename="/spinning-curiosity">
       <Routes>
-        <Route path="/login" element={<Login onLogin={setUser} />} />
-        <Route path="/scan" element={user ? <Scanner /> : <Navigate to="/login" />} />
-        <Route
-          path="/home"
-          element={user ? <ProductSearch /> : <Navigate to="/login" />}
-        />
-        <Route path="/" element={<Navigate to={user ? "/home" : "/login"} />} />
+        <Route path="/scan" element={<Scanner />} />
+        <Route path="/home" element={<ProductSearch />} />
+        <Route path="/" element={<Navigate to="/home" />} />
       </Routes>
     </Router>
   );
